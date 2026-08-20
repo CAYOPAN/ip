6c3688aebb4e760ec16d1c8a6afed2bfa9624480
CAYOPAN <calvinyopan07@gmail.com>
2026-08-20 10:05:37 +0800

    Rename Duke to Baymax

---
+++ src/main/java/Baymax.java
@@ -0,0 +1,14 @@
+public class Baymax {
+    public static void main(String[] args) {
+        String banner = """
+                BBBB   aaa   y   y  m     m   aaa   x   x
+                B   B a   a  y   y  mm   mm  a   a  x   x
+                B   B a   a   y y   m m m m  a   a   x x
+                BBBB  aaaaa    y    m  m  m  aaaaa    x
+                B   B a   a    y    m     m  a   a   x x
+                B   B a   a    y    m     m  a   a  x   x
+                BBBB  a   a    y    m     m  a   a  x   x
+                """;
+        System.out.print(banner);
+    }
+}
