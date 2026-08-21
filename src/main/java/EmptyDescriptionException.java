961e2c4016a17716fb239613f0a5db3659b83591
CAYOPAN <calvinyopan07@gmail.com>
2026-08-21 12:05:06 +0800

    Fix exception spacing

--- src/main/java/EmptyDescriptionException.java
+++ src/main/java/EmptyDescriptionException.java
@@ -1,5 +1,5 @@
 public class EmptyDescriptionException extends BaymaxException {
     public EmptyDescriptionException(String task) {
-        super("     OOPS!!! The description of a" + task + "cannot be empty.");
+        super("     OOPS!!! The description of a " + task + " cannot be empty.");
     }
 }
