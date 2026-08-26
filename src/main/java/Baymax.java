1801bb3f243c23f68d110145a66dcaa4e00a4737
CAYOPAN <calvinyopan07@gmail.com>
2026-08-26 15:26:29 +0800

    Extract application messages to Ui

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -24,35 +24,23 @@ public class Baymax {
     }
 
     public static void main(String[] args) {
-        System.out.print("""
-                ____________________________________________________________
-                BBBB   aaa   y   y  m     m   aaa   x   x
-                B   B a   a  y   y  mm   mm  a   a  x   x
-                B   B a   a   y y   m m m m  a   a   x x
-                BBBB  aaaaa    y    m  m  m  aaaaa    x
-                B   B a   a    y    m     m  a   a   x x
-                B   B a   a    y    m     m  a   a  x   x
-                BBBB  a   a    y    m     m  a   a  x   x
-                Hello! I'm Baymax. Your personal task companion.
-                What can I do for you?
-                ____________________________________________________________
-                """);
-
         Ui ui = new Ui();
         Storage storage = new Storage("./data/Baymax.txt");
         TaskList tasks = storage.load();
+
+        ui.showWelcome();
 
         while (ui.hasNextCommand()) {
             try {
                 String command = ui.readCommand();
 
-                System.out.println("____________________________________________________________");
+                ui.showSeparator();
                 if (command.equals("bye")) {
-                    System.out.println(" Bye. Hope to see you again soon!");
+                    ui.showGoodbye();
                     try {
                         storage.save(tasks);
                     } catch (IOException io) {
-                        System.out.println("Can not save tasks list. Previous tasks list can not be retrieve.");
+                        ui.showSaveError();
                     }
                     ui.close();
                     break;
@@ -172,7 +160,7 @@ public class Baymax {
             } catch (BaymaxException e) {
                 System.out.println(e.getMessage());
             } finally {
-                System.out.println("____________________________________________________________");
+                ui.showSeparator();
             }
 
         }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
  */
 public class Ui {
     private final Scanner scanner = new Scanner(System.in);
+    private static final String SEPARATOR =
+        "____________________________________________________________";
 
     /**
      * Checks whether another command is available.
@@ -29,5 +31,46 @@ public class Ui {
      */
     public void close() {
         scanner.close();
+    }
+
+    /**
+     * Displays Baymax's welcome message.
+     */
+    public void showWelcome() {
+        System.out.print("""
+            ____________________________________________________________
+            BBBB   aaa   y   y  m     m   aaa   x   x
+            B   B a   a  y   y  mm   mm  a   a  x   x
+            B   B a   a   y y   m m m m  a   a   x x
+            BBBB  aaaaa    y    m  m  m  aaaaa    x
+            B   B a   a    y    m     m  a   a   x x
+            B   B a   a    y    m     m  a   a  x   x
+            BBBB  a   a    y    m     m  a   a  x   x
+            Hello! I'm Baymax. Your personal task companion.
+            What can I do for you?
+            ____________________________________________________________
+            """);
+    }
+
+    /**
+     * Displays a separator between commands.
+     */
+    public void showSeparator() {
+        System.out.println(SEPARATOR);
+    }
+
+    /**
+     * Displays the goodbye message.
+     */
+    public void showGoodbye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Displays a storage error.
+     */
+    public void showSaveError() {
+        System.out.println(
+                "Can not save tasks list. Previous tasks list can not be retrieve.");
     }
 }
